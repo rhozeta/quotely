8116d9aeda7e1aa0f5d30c22d684eab19d4dc04a
Nicholas Mazzotta <nicholas.mazzotta@uoit.net>
2020-12-15 15:04:46 -0500

    test2314

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">QUOTELY</ion-title>
+          <ion-title size="large">I like your mom</ion-title>
         </ion-toolbar>
       </ion-header>
     
