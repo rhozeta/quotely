1b35bcbd2ff684f9a88cda6f913fc4e04e662278
Nicholas Mazzotta <nicholas.mazzotta@uoit.net>
2020-12-15 15:13:18 -0500

    hello

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
       <ion-header collapse="condense">
         <ion-toolbar>
 
-          <ion-title size="large">Hello my friend</ion-title>
+          <ion-title size="large">Hello my assholes`</ion-title>
 
         </ion-toolbar>
       </ion-header>
